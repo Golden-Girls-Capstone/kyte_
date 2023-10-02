bea74aaa9983168d6f39e39fabdd10764740fb8b
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-01 21:37:26 -0500

    built base Authentication service. moved css and img into static folder

--- src/main/java/com/kyterescue/services/AuthenticationService.java
+++ src/main/java/com/kyterescue/services/AuthenticationService.java
@@ -1,4 +1,33 @@
 package com.kyterescue.services;
 
+import com.kyterescue.entities.User;
+import com.kyterescue.entities.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service("authenticationService")
 public class AuthenticationService {
+    private User loggedInUser;
+    private final UserRepository usersDao;
+
+
+    AuthenticationService(UserRepository usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    public void grabAuthenticatedUserDetails(Model model) {
+        Object authenticatedUser = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(authenticatedUser instanceof UserDetails) {
+            User castedUser = (User) authenticatedUser;
+            long userId = castedUser.getId();
+            User loginUser = usersDao.getUserById(userId);
+            model.addAttribute("user", loginUser);
+        }
+    }
+
+    public User testCurrentSession() {
+        return loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
 }
