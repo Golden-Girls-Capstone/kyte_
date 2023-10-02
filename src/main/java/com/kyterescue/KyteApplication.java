e606450ad028d2d659c31fc811432fb2fff779f9
Emily Svetcos <emilysvetcos@gmail.com>
2023-10-02 11:02:14 -0500

    updated changes to navbar

--- src/main/java/com/kyterescue/KyteApplication.java
+++ src/main/java/com/kyterescue/KyteApplication.java
@@ -1,4 +1,4 @@
-package com.kyterescue.myapp;
+package com.kyterescue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/kyterescue/services/PetApiService.java
+++ src/main/java/com/kyterescue/services/PetApiService.java
@@ -1,25 +1,25 @@
-package com.kyterescue.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-@Service
-public class PetApiService {
-    @Value("${pet.token}")
-    private String petToken;
-    @Value("${pet.url}")
-    private String petURL;
-    private RestTemplate restTemplate;
-
-    @Autowired
-    public PetApiService(RestTemplate restTemplate, @Value("${pet.token}") String petToken, @Value("${pet.url}") String petURL) {
-        this.restTemplate = restTemplate;
-        this.petToken = petToken;
-        this.petURL = petURL;
-    }
-
-
-}
+//package com.kyterescue.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Service
+//public class PetApiService {
+//    @Value("${pet.token}")
+//    private String petToken;
+//    @Value("${pet.url}")
+//    private String petURL;
+//    private RestTemplate restTemplate;
+//
+//    @Autowired
+//    public PetApiService(RestTemplate restTemplate, @Value("${pet.token}") String petToken, @Value("${pet.url}") String petURL) {
+//        this.restTemplate = restTemplate;
+//        this.petToken = petToken;
+//        this.petURL = petURL;
+//    }
+//
+//
+//}
