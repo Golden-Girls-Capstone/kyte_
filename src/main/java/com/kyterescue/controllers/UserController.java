ce5c043de0f13d2d1d5524dcd14d2d2efd468b50
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-03 11:04:12 -0500

    signup/login controller methods built to allow user to signup/login

--- src/main/java/com/kyterescue/controllers/UserController.java
+++ src/main/java/com/kyterescue/controllers/UserController.java
@@ -1,28 +1,41 @@
 package com.kyterescue.controllers;
 
+import com.kyterescue.entities.User;
+import com.kyterescue.entities.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    UserRepository usersDao;
+    PasswordEncoder passwordEncoder;
+
+    UserController(UserRepository usersDao, PasswordEncoder passwordEncoder) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/signup")
-    public String viewSignup() {
+    public String viewSignup(Model model) {
+        model.addAttribute("user", new User());
         return "users/signup";
     }
 
     @PostMapping("/signup")
-    public String newUserSignup() {
+    public String newUserSignup(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(user);
         return "users/signup";
     }
 
     @GetMapping("/login")
     public String viewLogin() {
-        return "users/login";
-    }
-
-    @PostMapping("/login")
-    public String userLogsIn() {
         return "users/login";
     }
 
