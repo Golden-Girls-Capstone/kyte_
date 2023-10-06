729ff230269259125a58da8399ac789f61326f60
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-06 17:01:53 -0500

    worked api calls into backend with hardcoded zipcode

--- src/main/java/com/kyterescue/services/GrabAuthenticationTokenService.java
+++ src/main/java/com/kyterescue/services/GrabAuthenticationTokenService.java
@@ -1,36 +1,35 @@
-//package com.kyterescue.services;
-//
-//import okhttp3.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.IOException;
-//@Service
-//public class GrabAuthenticationTokenService {
-//    @Value("${pet.token}")
-//    private String petToken;
-//    @Value("${pet.secret}")
-//    private String petSecret;
-//    public String tokenURL = "https://api.petfinder.com/v2/oath2/token";
-//    final OkHttpClient client = new OkHttpClient();
-//
-//    GrabAuthenticationTokenService(@Value("${pet.token}") String petToken, @Value("${pet.secret}") String petSecret) {
-//        this.petToken = petToken;
-//        this.petSecret = petSecret;
-//    }
-//
-//   public String getBearerToken() throws IOException {
-//        RequestBody body = new FormBody.Builder()
-//                .add("token", "grant_type=client_credentials&client_id=" + petToken + "&client_secret=" + petSecret)
-//                .build();
-//        Request request = new Request.Builder()
-//                .url(tokenURL)
-//                .post(body)
-//                .build();
-//        try (Response response = client.newCall(request).execute()) {
-//            return response.body().string();
-//        }
-//    }
-//}
+package com.kyterescue.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kyterescue.entities.TokenMapper;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+@Service
+public class GrabAuthenticationTokenService {
+    @Value("${pet.token}")
+    private String petToken;
+    @Value("${pet.secret}")
+    private String petSecret;
+    public String tokenURL = "https://api.petfinder.com/v2/oauth2/token";
+    private OkHttpClient client;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public String getBearerToken() throws IOException {
+        RequestBody body = new FormBody.Builder()
+                .add("grant_type", "client_credentials")
+                .add("client_id", petToken)
+                .add("client_secret", petSecret)
+                .build();
+        Request request = new Request.Builder()
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .url(tokenURL)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            TokenMapper tokenMapper = mapper.readValue(response.body().string(), TokenMapper.class);
+            return tokenMapper.getAccess_token();
+        }
+    }
+}
