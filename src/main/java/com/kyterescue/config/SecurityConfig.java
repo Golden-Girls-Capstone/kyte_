d1053ae94af7d41cf5e23a2a8a247febad17744d
Emily Svetcos <emilysvetcos@gmail.com>
2023-10-10 11:48:19 -0500

    updates layout and some changes

--- src/main/java/com/kyterescue/config/SecurityConfig.java
+++ src/main/java/com/kyterescue/config/SecurityConfig.java
@@ -41,12 +41,12 @@ public class SecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
 //                         Pages that require authentication
 
-                        .requestMatchers("/profile", "/dashboard", "/profile/edit", "/profile/edit/delete/{id}", "pet/review/{fosterId}").authenticated()
+                        .requestMatchers("/profile",  "/profile/edit", "/profile/edit/delete/{id}", "pet/review/{fosterId}").authenticated()
 
                         /* Pages that do not require authentication
                          * anyone can visit the home page, register, login, and view ads */
 
-                        .requestMatchers("", "/", "/landing", "/sign-up", "/login", "/browse", "/api/test", "pets/**", "/logout", "/about", "/api/data/default", "/api/data/search", "/api/data/types", "/api/token").permitAll()
+                        .requestMatchers("", "/", "/dashboard", "/landing", "/sign-up", "/login", "/browse", "/api/test", "pets/**", "/logout", "/about", "/api/data/default", "/api/data/search", "/api/data/types", "/api/token").permitAll()
 
                         // allow loading of static resources
                         .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
