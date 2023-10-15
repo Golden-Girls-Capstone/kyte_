51e75e2a9a363548f61c56a51cd2ae36bcb63bc9
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-15 17:28:48 -0500

    scheduler built and tested

--- src/main/java/com/kyterescue/services/ScheduledCheckForFostersService.java
+++ src/main/java/com/kyterescue/services/ScheduledCheckForFostersService.java
@@ -2,11 +2,19 @@ package com.kyterescue.services;
 
 import com.kyterescue.entities.FosterPet;
 import com.kyterescue.entities.FosterPetRepository;
+import com.kyterescue.entities.User;
 import com.kyterescue.entities.UserRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Configuration
+@EnableScheduling
 public class ScheduledCheckForFostersService {
     private UserRepository usersDao;
     private FosterPetRepository fostersDao;
@@ -15,8 +23,17 @@ public class ScheduledCheckForFostersService {
         this.usersDao = usersDao;
         this.fostersDao = fostersDao;
     }
-    @Scheduled
+    @Scheduled(cron = "0 28 17 * * ?")
     public void scheduledCheckAndAssign() {
-
+        for(User user : usersDao.findAll()) {
+            List<FosterPet> fosterPets = fostersDao.findFosterPetsOfUser(user);
+            for(FosterPet pet : fosterPets) {
+                if(!pet.getStatus() && Objects.equals(pet.getStart_date(), LocalDate.now())) {
+                    pet.setStatus(true);
+                    fostersDao.save(pet);
+                }
+            }
+            usersDao.save(user);
+        }
     }
 }
