2da768d837dcb47bdab66a863ecb1b8930c5442d
SamR0429 <Samanatha.ramos01@gmail.com>
2023-10-17 14:01:05 -0500

    fixed badges service and made the seeder file for badges

---
+++ src/main/java/badge-seeder.sql
@@ -0,0 +1,11 @@
+use kyte_db;
+insert into badges(id, img, name)
+values (1, '1.svg', 'Fostered a barnyard animal'),
+       (2, '2.svg', 'Fostered a dog'),
+       (3, '3.svg', 'Fostered a rabbit'),
+       (4,'4.svg', 'Fostered a small furry animal'),
+       (5,'5.svg', 'Fostered a scaly friend'),
+       (6, '6.svg', 'Fostered a bird'),
+       (7,'7.svg', 'Fostered a cat'),
+       (8,'8.svg', 'Fostered a old friend');
+
