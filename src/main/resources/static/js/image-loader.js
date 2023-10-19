bdc4b76d6be5d385a2af9bdaac72cff707d915eb
Arusa Hussain <hussainarusa18@gmail.com>
2023-10-18 19:46:22 -0500

    added loader to website

--- src/main/resources/static/js/image-loader.js
+++ src/main/resources/static/js/image-loader.js
@@ -1,27 +1,9 @@
 
+window.onload = function () {
+    hideLoader();
+};
 
 function hideLoader() {
-    document.querySelector("#hamsterLoader").style.display ='none';
+    document.querySelector("#loading-ham").style.display = 'none';
+    document.querySelector("body").classList.remove("loading");
 }
-setTimeout(hideLoader, 3000);
-
-
-
-
-
-
-
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        // Add the "loading" class to the body element to apply the blur filter
-        document.querySelector("body").classList.add("loading");
-        document.querySelector("#loading-ham").style.visibility = "visible";
-            document.querySelector("#content-container").style.display = "block";
-    } else {
-        // Delay the execution of this code by 5 seconds (5000 milliseconds)
-        setTimeout(function() {
-            document.querySelector("#loading-ham").style.display = "none";
-            document.querySelector("body").classList.remove("loading");
-        }, 6000);
-    }
-};
