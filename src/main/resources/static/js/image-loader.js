a0d86c6e55f6864f42a190613e09192663f92588
Keila Vargas <keila.vargas0365@gmail.com>
2023-10-18 19:00:18 -0500

    added loaders

--- src/main/resources/static/js/image-loader.js
+++ src/main/resources/static/js/image-loader.js
@@ -1,25 +1,4 @@
-// window.addEventListener('load', function(e) {
-//     // e.preventDefault();
-// const loader = document.querySelector('#hamsterLoader');
-// const devCardContainer = document.querySelector ('.dev-card');
-//     // make loader appear
-//     // devCardContainer.style.display="none";
-//     loader.style.display="block";
-//
-//     // Hide the loader
-//     loader.style.display = "none";
-//     devCardContainer.style.display = "flex";
-// });
-// function hideLoader() {
-// //     // Hide the loader by setting its display property to 'none'
-//     document.getElementById('#hamsterLoader').style.display ='block';
-// //
-// }
-// //     setTimeout(hideLoader, 7000);
-//
-// document.onload = function() {
-//     setTimeout(hideLoader, 3000);
-// }
+
 
 function hideLoader() {
     document.querySelector("#hamsterLoader").style.display ='none';
@@ -28,3 +7,21 @@ setTimeout(hideLoader, 3000);
 
 
 
+
+
+
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        // Add the "loading" class to the body element to apply the blur filter
+        document.querySelector("body").classList.add("loading");
+        document.querySelector("#loading-ham").style.visibility = "visible";
+    } else {
+        // Delay the execution of this code by 5 seconds (5000 milliseconds)
+        setTimeout(function() {
+            document.querySelector("#loading-ham").style.display = "none";
+            document.querySelector("body").classList.remove("loading");
+            document.querySelector("#content-container").style.display = "block";
+        }, 3000);
+    }
+};
