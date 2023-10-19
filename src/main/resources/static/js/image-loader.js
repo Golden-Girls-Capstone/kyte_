60c2951d22b9d0f545c6f5ce91e03f67386ade48
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-18 19:41:50 -0500

    loading changes

--- src/main/resources/static/js/image-loader.js
+++ src/main/resources/static/js/image-loader.js
@@ -16,9 +16,9 @@ document.onreadystatechange = function() {
             // Add the "loading" class to the body element to apply the blur filter
             document.querySelector("body").classList.add("loading");
             document.querySelector("#loading-ham").style.visibility = "visible";
-            document.querySelector("#content-container").style.display = "block";
     } else {
         // Delay the execution of this code by 5 seconds (5000 milliseconds)
+            document.querySelector("#content-container").style.display = "block";
             document.querySelector("#loading-ham").style.display = "none";
             document.querySelector("body").classList.remove("loading");
     }
