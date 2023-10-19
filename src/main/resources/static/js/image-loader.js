6ce4034f9854bb2413c002c590b31f4fed5209fa
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-10-18 19:38:21 -0500

    loading changes

--- src/main/resources/static/js/image-loader.js
+++ src/main/resources/static/js/image-loader.js
@@ -3,7 +3,7 @@
 function hideLoader() {
     document.querySelector("#hamsterLoader").style.display ='none';
 }
-setTimeout(hideLoader, 3000);
+// setTimeout(hideLoader, 3000);
 
 
 
@@ -13,15 +13,13 @@ setTimeout(hideLoader, 3000);
 
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
-        // Add the "loading" class to the body element to apply the blur filter
-        document.querySelector("body").classList.add("loading");
-        document.querySelector("#loading-ham").style.visibility = "visible";
+            // Add the "loading" class to the body element to apply the blur filter
+            document.querySelector("body").classList.add("loading");
+            document.querySelector("#loading-ham").style.visibility = "visible";
             document.querySelector("#content-container").style.display = "block";
     } else {
         // Delay the execution of this code by 5 seconds (5000 milliseconds)
-        setTimeout(function() {
             document.querySelector("#loading-ham").style.display = "none";
             document.querySelector("body").classList.remove("loading");
-        }, 6000);
     }
 };
